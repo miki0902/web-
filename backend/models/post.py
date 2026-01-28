a9d8101736af8509a6fc907d5c6750d21710e184
Miki Nishikawa <miki0902@users.noreply.github.com>
2026-01-28 21:52:31 +0900

    Alembic（Migration）によるDBの属性の管理

--- backend/models/post.py
+++ backend/models/post.py
@@ -1,5 +1,7 @@
-from sqlalchemy import Column, Integer, String
+import datetime
+from sqlalchemy import Column, Integer, String, DateTime, func
 from database import Base
+from datetime import datetime
 
 class Post(Base):
     __tablename__ = "posts"
@@ -9,3 +11,9 @@ class Post(Base):
     body = Column(String, index=True)
     userId = Column(Integer, index=True)
     
+    created_at = Column(DateTime, default=datetime.utcnow)#属性追加日時を記録
+    updated_at = Column(DateTime, default=datetime.utcnow, onupdate=func.now()) #属性更新日時を記録
+    """
+    alembic revision --autogenerate -m "add created_at to posts"
+    alembic upgrade head でデータベースに反映される。
+    """
